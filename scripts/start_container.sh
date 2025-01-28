b099cbbf32e546f02a54e3a24186a3cc8bf9c1d7
S  D <5098981+spooux@users.noreply.github.com>
2025-01-28 22:35:43 +0530

    codedeploy pull fix

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull spoorthyd/simple-python-flask-app
+docker pull spoorthyd/simple-python-flask-app:latest
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 spoorthyd/simple-python-flask-app
