4e438523a7a468482f0533eb60451749e6d7ed70
S  D <5098981+spooux@users.noreply.github.com>
2025-01-29 16:26:20 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 set -e
 
-# Get the container ID of the running container (with sudo)
-containerid=$(sudo docker ps -q --filter "name=dreamy_darwin")
-
-# Debugging: Check if containerid is empty
-echo "Container ID: $containerid"
-
-# Check if a container ID was found
-if [ -z "$containerid" ]; then
-  echo "No running container found with the name 'dreamy_darwin'."
-else
-  # Stop the container if it's running
-  echo "Stopping container with ID: $containerid"
-  sudo docker stop "$containerid"
-fi
+echo "test"
 
