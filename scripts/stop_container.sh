7eb30a40f2f488dcfc711c0762ff1fae19a3d8b0
S  D <5098981+spooux@users.noreply.github.com>
2025-01-29 16:21:06 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
 
-# Get the container ID of the running container
+# Get the container ID of the running container (with sudo)
 containerid=$(sudo docker ps -q --filter "name=dreamy_darwin")
+
+# Debugging: Check if containerid is empty
+echo "Container ID: $containerid"
 
 # Check if a container ID was found
 if [ -z "$containerid" ]; then
