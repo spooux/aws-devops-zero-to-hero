4a673f2bef8bc466e130848a86aeb0d0dd38db30
S  D <5098981+spooux@users.noreply.github.com>
2025-01-30 12:00:54 +0530

    add files Update start_container.sh

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -2,7 +2,9 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull spoorthyd/simple-python-flask-app:tagname
+
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 spoorthyd/simple-python-flask-app:tagname
+
